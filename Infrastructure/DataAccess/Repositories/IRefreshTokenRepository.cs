70fac1dad041226fde41bd1d20c63df7790887fd
Seif357 <seifomar3579@gmail.com>
2026-02-06 02:10:41 +0200

    Restart project

--- Infrastructure/DataAccess/Repositories/IRefreshTokenRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Domain.Models;
-using Domain.Models.Auth;
-
-namespace Infrastructure.DataAccess.Repositories;
-
-public interface IRefreshTokenRepository : IGenericRepository<RefreshToken>
-{
-
-}
--- Infrastructure/DataAccess/Repositories/RefreshTokensRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿using Domain.Models;
-using Domain.Models.Auth;
-
-namespace Infrastructure.DataAccess.Repositories;
-
-public class RefreshTokensRepository(AppDbContext context) : GenericRepository<RefreshToken>(context), IRefreshTokenRepository;
