aebc926a2734cf459bfd2b3465bc24a5f13445c1
Seif357 <seifomar3579@gmail.com>
2026-02-06 02:11:56 +0200

    Add: Base user

---
+++ Domain/Models/Auth/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models.Auth
+{
+    public class AppUser: IdentityUser<int>
+    { 
+        public gender Gender { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Domain/Models/Auth/gender.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Models.Auth
+{
+    public enum gender
+    {
+        male = 1,
+        female,
+    }
+}
