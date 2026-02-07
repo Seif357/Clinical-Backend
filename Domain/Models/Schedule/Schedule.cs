7523e2399592c29cd7db9e89927889ed2a59c6ac
Seif357 <seifomar3579@gmail.com>
2026-02-07 17:14:43 +0200

    Add: Schedule Models For Future Work

---
+++ Domain/Models/Schedule/Schedule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models.Schedule
+{
+    public class Schedule : ParentEntity
+    {
+        public int DoctorId { get; set; }
+        public ICollection<ScheduleSlot> ScheduleSlots { get; set; }
+        public ScheduleSlot ScheduleSlot { get; set; }
+
+    }
+}
---
+++ Domain/Models/Schedule/ScheduleSlot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models.Schedule
+{
+    public class ScheduleSlot: ParentEntity
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int ScheduleId { get; set; }
+        public int? patientId { get; set; }
+        public bool IsBooked => patientId != null;
+    }
+}
