a8f557993bfb34c94c29dc9b6e0490d65af1aa9b
Seif357 <seifomar3579@gmail.com>
2026-02-07 17:18:27 +0200

    Add: AI Model Entities

---
+++ Domain/Models/AI/ModelInput.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models.AI
+{
+    public class ModelInput
+    {
+        public string HistopathologyImagePath { get; set; }
+    }
+}
---
+++ Domain/Models/AI/ModelOutput.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models.AI
+{
+    public class ModelOutput
+    {
+        public string Classification { get; set; }
+    }
+}
