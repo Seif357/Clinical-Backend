8818a0c0c2635b0c3826f2136b95ba1e677bcf0f
Seif357 <seifomar3579@gmail.com>
2026-02-11 01:33:42 +0200

    Add: Scalar

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,10 @@
 using System.Reflection;
 using Application;
 using Infrastructure;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.FileProviders;
+using Scalar.AspNetCore;
 
 namespace API
 {
@@ -14,10 +16,17 @@ namespace API
 
             builder.Services.AddControllers();
             builder.Services.AddInfrastructure(builder.Configuration);
+            builder.Services.AddOpenApi();
             var app = builder.Build();
+            app.MapOpenApi();
 
+            app.Map("/", () => Results.Redirect("/scalar/v1", permanent: true));
             app.UseHttpsRedirection();
-
+            app.MapScalarApiReference(options =>
+            {
+                options.WithTitle("My API Documentation")
+                       .WithTheme(ScalarTheme.Purple);
+            });
             app.UseAuthorization();
 
             app.MapControllers();
