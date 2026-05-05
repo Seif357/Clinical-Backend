d9f7b9df1bc40c49ff9d82570f63eaef12aa3eb7
seif357 <seifomar3579@gmail.com>
2026-05-05 14:58:57 +0300

    Refactor: PrescribedMedication Model
    Change-Id: I9a084045211e9f630893751da9344a450d53d7da

--- Domain/Models/MedicalRecordAttributes/PrescribedMedication.cs
+++ Domain/Models/MedicalRecordAttributes/PrescribedMedication.cs
@@ -1,10 +1,11 @@
 ﻿namespace Domain.Models.MedicalRecordAttributes;
 
-public class PrescribedMedication : ParentEntity
+public class PrescribedMedication : ReviewableEntry
 {
     public string Name { get; set; }
     public string Dosage { get; set; }
     public string Frequency { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime? EndDate { get; set; }
+    public int MedicalRecordId { get; set; }
 }
