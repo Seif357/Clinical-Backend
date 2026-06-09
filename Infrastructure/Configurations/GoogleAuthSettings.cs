6126f0895efb910c100bf683f18a084a099e52b0
seif357 <seifomar3579@gmail.com>
2026-06-09 07:11:12 +0300

    feat: Allow Google Auth for mobile
    Change-Id: I938d6b81a32d7aee6860aad4e4399efc1a47d9a4

--- Infrastructure/Configurations/GoogleAuthSettings.cs
+++ Infrastructure/Configurations/GoogleAuthSettings.cs
@@ -4,4 +4,5 @@ public class GoogleAuthSettings
 {
     public const string SectionName = "GoogleAuth";
     public string ClientId { get; set; } = string.Empty;
+    public string AndroidClientId { get; set; } = string.Empty;
 }
